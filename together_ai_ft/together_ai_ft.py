9f69c43ca2d85162cc46e68a2363f1eea6788275
Haofei Yu <1125027232@qq.com>
2023-10-10 20:09:35 -0400

    close together ai issue (#15)
    Co-authored-by: Ruiyi Wang <ruiyi.pamela.wang@gmail.com>

--- together_ai_ft/together_ai_ft.py
+++ together_ai_ft/together_ai_ft.py
@@ -1,22 +1,30 @@
 import together
 import os
 
-resp = together.Files.upload(file="together_ai_data.jsonl")
-file_id = resp["id"]
-files_list = together.Files.list()
+together.api_key = ""
 
+
+data_path = "/Users/pamela/Documents/capstone/sotopia-ft-data/human-bot-train-gpt4-gpt4-easy-partial.jsonl"
+model_type = "togethercomputer/llama-2-13b-chat"
+
+resp = together.Files.check(file=data_path, model=model_type)
+resp = together.Files.upload(file=data_path, model=model_type)
+file_id = resp["id"]
+# print(file_id)
+# files_list = together.Files.list()
+# print(files_list['data'])
 
 resp = together.Finetune.create(
-    training_file = files_list['data'][0]['id'],
-    model = 'togethercomputer/llama-2-7b-chat',
-    n_epochs = 3,
-    n_checkpoints = 1,
-    batch_size = 4,
-    learning_rate = 1e-5,
-    suffix = 'test-finetune',
-    wandb_api_key = '972035264241fb0f6cc3cab51a5d82f47ca713db',
+    # training_file = files_list['data'][0]['id'],
+    training_file=file_id,
+    model=model_type,
+    n_epochs=1,
+    n_checkpoints=1,
+    batch_size=8,
+    learning_rate=5e-5,
+    suffix='sotopia-finetune-GPT4+GPT4-clean-tag-1',
+    wandb_api_key='eca44f65849afa1cc146c22631b0b5001ccd24d7',
 )
 
 fine_tune_id = resp['id']
 print(resp)
-
