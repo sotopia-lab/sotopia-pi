1e44e745573bb1f52c7cf3a3faf8ca7f17b4dfab
Haofei Yu <1125027232@qq.com>
2023-10-20 18:44:12 -0400

    support inference on the whole dataset (#66)

--- llm_ft/mistral-inference.sh
+++ llm_ft/mistral-inference.sh
@@ -1,1 +1,1 @@
-python3 -m fastchat.serve.cli --model-path ./checkpoint-ft/checkpoint-4525 --conv-template "vicuna_v1.1"
+python3 -m fastchat.serve.inf --model-path ./Mistral-7B-v0.1 --conv-template "vicuna_v1.1"
