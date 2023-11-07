80a3b65621628eb7a835ea5dbba7322b39ec3fc5
Jasonqi146 <jasonqi146@gmail.com>
2023-11-06 20:05:04 -0500

    added llama-factory under llm_rl

---
+++ llm_rl/reward_model.sh
@@ -0,0 +1,21 @@
+python src/train_bash.py \
+    --stage rm \
+    --model_name_or_path meta-llama/Llama-2-13b \
+    --do_train \
+    --dataset comparison_gpt4_en \
+    --template default \
+    --finetuning_type lora \
+    --lora_target q_proj,v_proj \
+    --resume_lora_training False \
+    --checkpoint_dir ./llama-2-13b-rm \
+    --output_dir ./llama-2-13b-rm \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --learning_rate 1e-6 \
+    --num_train_epochs 1.0 \
+    --plot_loss \
+    --fp16 \
+    --hf_auth_token "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"
---
+++ llm_rl/src/llmtuner/webui/interface.py
@@ -0,0 +1,66 @@
+import gradio as gr
+from transformers.utils.versions import require_version
+
+from llmtuner.webui.components import (
+    create_top,
+    create_train_tab,
+    create_eval_tab,
+    create_infer_tab,
+    create_export_tab,
+    create_chat_box
+)
+from llmtuner.webui.common import save_config
+from llmtuner.webui.css import CSS
+from llmtuner.webui.engine import Engine
+
+
+require_version("gradio>=3.38.0,<4.0.0", "To fix: pip install \"gradio>=3.38.0,<4.0.0\"")
+
+
+def create_ui() -> gr.Blocks:
+    engine = Engine(pure_chat=False)
+
+    with gr.Blocks(title="LLaMA Board", css=CSS) as demo:
+        engine.manager.all_elems["top"] = create_top()
+        lang: "gr.Dropdown" = engine.manager.get_elem_by_name("top.lang")
+
+        with gr.Tab("Train"):
+            engine.manager.all_elems["train"] = create_train_tab(engine)
+
+        with gr.Tab("Evaluate"):
+            engine.manager.all_elems["eval"] = create_eval_tab(engine)
+
+        with gr.Tab("Chat"):
+            engine.manager.all_elems["infer"] = create_infer_tab(engine)
+
+        with gr.Tab("Export"):
+            engine.manager.all_elems["export"] = create_export_tab(engine)
+
+        demo.load(engine.resume, outputs=engine.manager.list_elems())
+        lang.change(engine.change_lang, [lang], engine.manager.list_elems(), queue=False)
+        lang.input(save_config, inputs=[lang], queue=False)
+
+    return demo
+
+
+def create_web_demo() -> gr.Blocks:
+    engine = Engine(pure_chat=True)
+
+    with gr.Blocks(title="Web Demo", css=CSS) as demo:
+        lang = gr.Dropdown(choices=["en", "zh"])
+        engine.manager.all_elems["top"] = dict(lang=lang)
+
+        chat_box, _, _, chat_elems = create_chat_box(engine, visible=True)
+        engine.manager.all_elems["infer"] = dict(chat_box=chat_box, **chat_elems)
+
+        demo.load(engine.resume, outputs=engine.manager.list_elems())
+        lang.change(engine.change_lang, [lang], engine.manager.list_elems(), queue=False)
+        lang.input(save_config, inputs=[lang], queue=False)
+
+    return demo
+
+
+if __name__ == "__main__":
+    demo = create_ui()
+    demo.queue()
+    demo.launch(server_name="0.0.0.0", server_port=7860, share=False, inbrowser=True)
