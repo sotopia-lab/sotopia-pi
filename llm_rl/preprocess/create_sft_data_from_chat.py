b1bda1e0ef16c3189fad74bcc637903ed7ca1be4
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2023-11-08 01:13:05 -0500

    Feature/llama factory llama2 pipeline (#89)
    * added llama-factory under llm_rl
    
    * added sft training bash
    
    * added datasets from llama-factory; will delete later
    
    * finished llama-2-13b train and inference
    
    * fixed minor errors
    
    * changed config
    
    * added deepspeed config
    
    * added more training config to train bash
    
    * adding fix for wandb tags and distributed ranks
    
    * added fastchat data to replicate training for 2k
    
    (cherry picked from commit c7696384afb48a145318dfdfbf23e297d06d1d19)

---
+++ llm_rl/preprocess/create_sft_data_from_chat.py
@@ -0,0 +1,17 @@
+import json
+
+with open("./fastchat-ft-gp4-gpt4-easy-truncated.json", 'r') as f:
+    data = json.load(f)
+
+result = []
+for dp in data:
+    new_dp = {}
+    convs = dp['conversations']
+    new_dp['instruction'] = convs[0]['value']
+    new_dp['input'] = ""
+    new_dp['output'] = convs[1]['value']
+    
+    result.append(new_dp)
+
+with open("../data/fastchat-ft-gp4-gpt4-easy-truncated.json", 'w') as f:
+    json.dump(result, f, indent=4)
