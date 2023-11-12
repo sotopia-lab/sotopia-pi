25d265adf1d8471e250e16e38d16e6c917454f85
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2023-11-11 21:08:34 -0500

    llama factory llama2-13b and mistral-7b pipeline (#93)
    * added llama-factory under llm_rl
    
    * added sft training bash
    
    * added datasets from llama-factory; will delete later
    
    * finished llama-2-13b train and inference
    
    * fixed minor errors
    
    * changed config
    
    * added deepspeed config
    
    * added more training config to train bash
    
    * adding fix for wandb tags and distributed ranks
    
    * added fastchat data to replicate training for 2k
    
    * tyring to replicate fastchat as close as possible
    
    * before merging
    
    * changed finetue scripts for better performance
    
    * added new data
    
    * example bash
    
    * example bash for mistral
    
    (cherry picked from commit 0c53e37b6b3a6dc2bc970f475be37bead4166885)

--- llm_rl/finetune-mistral-7b.sh
+++ llm_rl/finetune-mistral-7b.sh
@@ -1,9 +1,8 @@
 deepspeed src/train_bash.py \
     --stage sft \
     --model_name_or_path mistralai/Mistral-7B-v0.1 \
-    --dataset dummy_convs \
+    --dataset sotopia_no_slide_no_filter_format_sft \
     --dataset_dir ./data/ \
-    --val_size 0.1 \
     --cutoff_len 4096 \
     --template llama2-sotopia \
     --wandb_project "llama-factory-sft" \
@@ -11,8 +10,8 @@ deepspeed src/train_bash.py \
     --use_fast_tokenizer False \
     --do_train \
     --num_train_epochs 15.0 \
-    --per_device_train_batch_size 8 \
-    --gradient_accumulation_steps 8 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 32 \
     --finetuning_type lora \
     --lora_target q_proj,v_proj \
     --learning_rate 5e-5 \
@@ -21,19 +20,15 @@ deepspeed src/train_bash.py \
     --warmup_ratio 0.03 \
     --quantization_bit 4 \
     --quantization_type nf4 \
-    --double_quantization \
+    --double_quantization True \
     --flash_attn True \
     --gradient_checkpointing True \
-    --bf16 \
-    --tf32 True \
+    --bf16 True \
     --cache_dir ./model_cache \
     --overwrite_cache \
     --output_dir ./mistral-7b-sft_cache \
     --overwrite_output_dir \
     --logging_steps 1 \
-    --evaluation_strategy "steps" \
-    --per_device_eval_batch_size 32 \
-    --eval_accumulation_steps 32 \
     --save_strategy "epoch" \
     --save_total_limit 5 \
     --use_auth_token True \
@@ -42,6 +37,10 @@ deepspeed src/train_bash.py \
     --deepspeed ./deepspeed_config_s2.json
 
     # --dataset alpaca_gpt4_en \
+    # --val_size 0.1 \
+    # --evaluation_strategy "steps" \
+    # --per_device_eval_batch_size 32 \
+    # --eval_accumulation_steps 32 \
     # --lora_rank 8 \
     # --lora_alpha 16 \
     # --lora_dropout 0.05 \
