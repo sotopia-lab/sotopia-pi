27dfdbea6d4bb0cd2ef6a648ddeb7d16f0beb257
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2023-11-25 19:32:50 -0500

    Added basic backbone pipeline (#120)
    * Added basic backbone pipeline
    
    * changed package structure; fixed minor errors to work on cuda
    
    (cherry picked from commit a99b5cbf62f54007f4b3e8b213413ecebc46a167)

---
+++ llm_self_train/pipelines/run_train.py
@@ -0,0 +1,22 @@
+from pipelines import config
+
+import yaml
+import subprocess
+
+with open('./train_args.yml', 'r') as file:
+    train_args = yaml.safe_load(file)
+
+def run():
+    args = ["deepspeed", f"--num_gpus={config['num_gpus']}", "../llm_rl/src/train_bash.py"]
+    for key, value in train_args.items():
+        if key in config:
+            value = config[key]
+            
+        if isinstance(value, bool):
+            if value:
+                args.append(f"--{key}")
+        else:
+            args.append(f"--{key}")
+            args.append(str(value))
+
+    subprocess.run(args)
