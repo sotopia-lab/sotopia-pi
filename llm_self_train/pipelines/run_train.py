68f1c39dd25084518fc3eb57d4be9f3c402984da
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2023-12-05 13:13:36 -0500

    Feature/async runs (#127)
    * starting async runs
    
    * fixed async problems
    
    * minor fixes
    
    * added gitignore
    
    (cherry picked from commit 3d4741a53eabd9681f7c1915934c452cee6c7cb7)

--- llm_self_train/pipelines/run_train.py
+++ llm_self_train/pipelines/run_train.py
@@ -6,7 +6,7 @@ import subprocess
 with open('./resources/train_args.yml', 'r') as file:
     train_args = yaml.safe_load(file)
 
-def run():
+def run_sft():
     args = ["deepspeed", f"--num_gpus={config['num_gpus']}", "../llm_rl/src/train_bash.py"]
     for key, value in train_args.items():
         if key in config:
