a7f67e987ee615496994a602685776974462864d
Jasonqi146 <jasonqi146@gmail.com>
2023-12-06 10:01:20 +0000

    finished cloud utils

--- llm_self_train/gcp_demo.py
+++ llm_self_train/gcp_demo.py
@@ -1,19 +1,19 @@
-from pipelines.gcp_util import upload_to_gcp, download_from_gcp, monitor_and_upload
+from pipelines.cloud_util import upload_dir_to_gcp, download_dir_from_gcp, list_blobs_with_prefix, monitor_and_upload
 import asyncio
 import time
 
-object_location = '/Users/zhengyangqi/Desktop/template-demo.txt'  # Replace with your file path
-oauth2_token_location = './resources/gcp_auth.token'      # Replace with your OAuth2 token
-content_type = 'application/json; charset=utf-8'      # Replace with the content type of your object
-bucket_name = 'pipeline-test-storage'        # Replace with your bucket name
-object_name = 'test/test.txt'        # Replace with your object name
-save_to_location = './test.txt'
+bucket_name = 'pipeline-test-storage' 
 
-# Call the upload function
-response = upload_to_gcp(object_name, object_location, oauth2_token_location, bucket_name, content_type)
-print(response.text)
-response = download_from_gcp(object_name, save_to_location, oauth2_token_location, bucket_name)
-print(response.text)
+source_dir = '/workspace/sotopia-llm/llm_self_train/output_cache/checkpoint-960'
+dest_blob_prefix = 'checkpoint-960'
+
+source_blob_prefix = 'checkpoint-960'
+dest_dir = '/workspace/sotopia-llm/llm_self_train/checkpoint-hello'
+
+# Call the upload and download function
+upload_dir_to_gcp(source_dir, dest_blob_prefix, bucket_name)
+download_dir_from_gcp(source_blob_prefix, dest_dir, bucket_name)
+print(list_blobs_with_prefix(bucket_name, ""))
 
 run_sft_completed = False
 
@@ -21,14 +21,14 @@ def should_stop():
     global run_sft_completed
     return run_sft_completed
 
-async def timer():
-    await asyncio.sleep(15)
+async def timer(timeout=60*2):
+    await asyncio.sleep(timeout)
     print("Stopping...")
     global run_sft_completed
     run_sft_completed = True
     
-async def hello():
+async def main():
     await asyncio.gather(monitor_and_upload('./demo_cache', 5, should_stop=should_stop), timer())
 
-asyncio.run(hello())
+asyncio.run(main())
 print("Done")
