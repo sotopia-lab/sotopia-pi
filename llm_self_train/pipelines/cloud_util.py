31355ee671eb7107e9801c6924a7568aae6499c2
Jasonqi146 <jasonqi146@gmail.com>
2023-12-06 10:01:20 +0000

    finished cloud utils
    (cherry picked from commit 5379bd310df8c94b32d1c245d1e26130d2f03136)

--- llm_self_train/pipelines/cloud_util.py
+++ llm_self_train/pipelines/cloud_util.py
@@ -1,20 +1,71 @@
 from pipelines import config
-
 import os
-import requests
-import zipfile
-
 import asyncio
-import os
+from google.cloud.storage import Client, transfer_manager
+from tqdm import tqdm
 
-async def monitor_and_upload(directory_to_monitor, check_interval=60, should_stop=lambda: False, oauth2_token_location=config['oauth2_token_location'], bucket_name=config['bucket_name']):
+def upload_dir_to_gcp(source_dir, destination_blob_prefix, bucket_name=config['bucket_name']):
+    ''' 
+    Uploads a directory to a given destination in GCP storage.
+    
+    source_dir: The local directory to upload.
+    destination_blob_prefix: The destination directory in GCP storage.
+    bucket_name: The name of the bucket to upload to.
+    '''
+    storage_client = Client()
+    bucket = storage_client.bucket(bucket_name)
+    files_to_upload = [os.path.join(root, file) for root, _, files in os.walk(source_dir) for file in files]
+        
+    for local_file_path in tqdm(files_to_upload, desc="Uploading files"):
+        relative_path = os.path.relpath(local_file_path, source_dir)
+        blob_path = os.path.join(destination_blob_prefix, relative_path)
+        blob = bucket.blob(blob_path)
+        blob.upload_from_filename(local_file_path)
+        
+        
+def download_dir_from_gcp(source_blob_prefix, destination_dir, bucket_name=config['bucket_name']):
+    '''
+    Downloads a directory from a given source in GCP storage.
+    
+    source_blob_prefix: The source directory in GCP storage.
+    destination_dir: The local directory to download to.
+    bucket_name: The name of the bucket to download from.
+    '''
+    storage_client = Client()
+    bucket = storage_client.bucket(bucket_name)
+    
+    blobs = list(bucket.list_blobs())
+    blob_paths = [blob.name for blob in blobs]
+    
+    for blob_path in tqdm(blob_paths, desc="Downloading files"):
+        local_file_path = os.path.join(destination_dir, os.path.relpath(blob_path, source_blob_prefix))
+        os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+        
+        blob = bucket.blob(blob_path)
+        blob.download_to_filename(local_file_path)
+        
+def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
+    '''
+    Lists all blobs in a given bucket with a given prefix.
+    
+    bucket_name: The name of the bucket to list blobs from.
+    prefix: The prefix to filter blobs by.
+    delimiter: The delimiter to use for filtering blobs.
+    '''
+    storage_client = Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    blobs = bucket.list_blobs(prefix=prefix, delimiter=delimiter)
+    return [blob.name for blob in blobs]
+
+
+async def monitor_and_upload(directory_to_monitor, check_interval=60, should_stop=lambda: False, bucket_name=config['bucket_name']):
     '''
     Monitors a directory and uploads new subdirectories to GCP
 
     Parameters:
         directory_to_monitor (str): Directory to monitor for new subdirectories
         gcp_bucket_name (str): GCP bucket name for uploading
-        oauth2_token_location (str): The location of the OAuth2 token
         check_interval (int): Time interval (in seconds) to check for new subdirectories
     '''
     already_uploaded = set()
@@ -34,86 +85,13 @@ async def monitor_and_upload(directory_to_monitor, check_interval=60, should_sto
                     await asyncio.sleep(check_interval)
                 
             for subdir in new_subdirectories:
+                if should_stop() == True:
+                    return
                 subdir_path = os.path.join(directory_to_monitor, subdir)
-                zip_name = f"{subdir}.zip"
-                
-                print(f"Zipping {subdir}...")
-                zip_directory(subdir_path, zip_name)
-                print(f"Zipped {subdir}.")
-                
-                print(f"Uploading {zip_name} to GCP...")
-                response = upload_to_gcp(zip_name, zip_name)
+                print(f"Uploading {subdir} to GCP...")
+                upload_dir_to_gcp(subdir_path, subdir)
                 already_uploaded.add(subdir)
                 print(f"Uploaded {subdir} to GCP.")
-                print(response.text)
 
         except Exception as e:
             print(f"An error occurred: {e}")
-
-
-def upload_to_gcp(object_name, object_location, oauth2_token_location=config['oauth2_token_location'], bucket_name=config['bucket_name'], content_type="application/json; charset=utf-8"):
-    '''
-    Uploads a file to a bucket in Google Cloud Storage
-    
-    Parameters:
-        object_location (str): The location of the file to upload
-        oauth2_token_location (str): The location of the OAuth2 token
-        content_type (str): The content type of the object
-        bucket_name (str): The name of the bucket to upload to
-        object_name (str): The name of the object to upload
-    '''
-    url = f'https://storage.googleapis.com/upload/storage/v1/b/{bucket_name}/o?uploadType=media&name={object_name}'
-    with open(oauth2_token_location, 'r') as file:
-        oauth2_token = str(file.read())
-    
-    headers = {
-        'Authorization': f'Bearer {oauth2_token}',
-        'Content-Type': content_type
-    }
-
-    with open(object_location, 'rb') as file:
-        data = file.read()
-
-    response = requests.post(url, headers=headers, data=data)
-    return response
-
-
-def download_from_gcp(object_name, save_to_location, oauth2_token_location=config['oauth2_token_location'], bucket_name=config['bucket_name']):
-    '''
-    Downloads a file from a bucket in Google Cloud Storage
-    
-    Parameters:
-        oauth2_token_location (str): The location of the OAuth2 token
-        bucket_name (str): The name of the bucket to download from
-        object_name (str): The name of the object to download
-        save_to_location (str): The location to save the file to
-    '''
-    if '/' in object_name:
-        object_name = object_name.replace('/', '%2F')
-        
-    url = f'https://storage.googleapis.com/storage/v1/b/{bucket_name}/o/{object_name}?alt=media'
-    
-    with open(oauth2_token_location, 'r') as file:
-        oauth2_token = str(file.read())
-        
-    headers = {
-        'Authorization': f'Bearer {oauth2_token}'
-    }
-
-    response = requests.get(url, headers=headers)
-    with open(save_to_location, 'wb') as file:
-        file.write(response.content)
-    return response
-
-def zip_directory(folder_path, zip_name):
-    with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for root, dirs, files in os.walk(folder_path):
-            for file in files:
-                # Create a relative path for files to keep the directory structure
-                relative_path = os.path.relpath(os.path.join(root, file), os.path.dirname(folder_path))
-                zipf.write(os.path.join(root, file), arcname=relative_path)
-
-
-def unzip_directory(zip_name, folder_path):
-    with zipfile.ZipFile(zip_name, 'r') as zipf:
-        zipf.extractall(folder_path)
