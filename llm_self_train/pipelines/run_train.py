0cace0114e293325dbbd6ccce1cfbc20344adae3
Jasonqi146 <jasonqi146@gmail.com>
2023-12-10 18:39:26 -0500

    need test

--- llm_self_train/pipelines/run_train.py
+++ llm_self_train/pipelines/run_train.py
@@ -6,11 +6,13 @@ import subprocess
 with open('./resources/train_args.yml', 'r') as file:
     train_args = yaml.safe_load(file)
 
-def run_sft():
+def run_sft(output_dir):
     args = ["deepspeed", f"--num_gpus={config['num_gpus']}", "../llm_rl/src/train_bash.py"]
     for key, value in train_args.items():
         if key in config:
             value = config[key]
+        if key == 'output_dir':
+            value = output_dir
             
         if isinstance(value, bool):
             if value:
