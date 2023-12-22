9124dea56874517d023a85eeb46398d4f0f32ee1
Haofei Yu <1125027232@qq.com>
2023-12-22 19:38:17 +0800

    switch to sync and make sure generate two social goals

---
+++ data_generate/openai_test.py
@@ -0,0 +1,11 @@
+import openai
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
