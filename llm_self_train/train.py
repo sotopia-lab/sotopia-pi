924032e9fbc7054b9919f036b910a6250d738bc5
Jasonqi146 <jasonqi146@gmail.com>
2023-12-23 17:33:49 -0500

    changed custom callback to save full model

--- llm_self_train/train.py
+++ llm_self_train/train.py
@@ -1,6 +1,6 @@
 from pipelines.pull_data import preprocess_episodes_with_tag
 from pipelines.run_train import run_sft
-from pipelines.monitor_proc import monitor_local_and_upload_to_gcp
+from llm_self_train.pipelines.monitor_processes import monitor_local_and_upload_to_gcp
 
 import os
 import multiprocessing
@@ -14,6 +14,8 @@ def should_stop(run_sft_completed):
     return run_sft_completed.value
 
 def main():
+    os.umask(0o000)
+
     if not os.path.exists("../llm_rl/data/sotopia_custom_training_sft.json"):
         preprocess_episodes_with_tag()
 
@@ -23,14 +25,11 @@ def main():
         print(curr_improve_dir)
 
         sft_process = multiprocessing.Process(target=run_sft, args=(curr_improve_dir, ))
-        monitor_process = multiprocessing.Process(target=monitor_local_and_upload_to_gcp, args=(curr_improve_dir, f"{config['experiment_name']}/improve-{i}", 60, lambda: should_stop(run_sft_completed)))
         
         sft_process.start()
-        monitor_process.start()
         
         sft_process.join()
         run_sft_completed.value = True
-        monitor_process.join()
 
 if __name__ == "__main__":
     main()
