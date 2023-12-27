d808c003170cc74f1887258eaa523a157f14b439
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2023-12-27 03:07:40 -0500

    Feature/flatten checkpoint names (#141)
    * changed custom callback to save full model
    
    * accomodate deploy

--- llm_rl/src/llmtuner/tuner/sft/custom_callback.py
+++ llm_rl/src/llmtuner/tuner/sft/custom_callback.py
@@ -3,10 +3,11 @@ from transformers import TrainerCallback
 from llmtuner.tuner.core.utils import is_first_node
 
 class SaveModelCallback(TrainerCallback):
-    def __init__(self, save_epochs, output_dir, checkpoint_saved_queue):
+    def __init__(self, save_epochs, output_dir, checkpoint_saved_queue, improve_step):
         self.save_epochs = save_epochs
         self.output_dir = output_dir
         self.checkpoint_saved_queue = checkpoint_saved_queue
+        self.curr_improve_step = improve_step
         self.curr_epoch = 0
 
     def on_epoch_end(self, args, state, control, model=None, **kwargs):
@@ -22,7 +23,7 @@ class SaveModelCallback(TrainerCallback):
             return
         
         # Customized checkpoint name
-        custom_checkpoint_name = f"checkpoint_epoch_{int(self.curr_epoch)}"
+        custom_checkpoint_name = f"checkpoint_improve-{self.curr_improve_step}_epoch-{int(self.curr_epoch)}"
         
         # Original auto-saved checkpoint directory
         auto_checkpoint_dir = os.path.join(args.output_dir, f"checkpoint-{state.global_step}")
