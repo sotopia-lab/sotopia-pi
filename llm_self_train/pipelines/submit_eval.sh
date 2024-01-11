0c5ccb49e7a79af61c1291a53b132276c7b516cc
Ruiyi Wang <ruiyi.pamela.wang@gmail.com>
2024-01-11 18:23:52 -0500

    Fix bugs for selftrain

--- llm_self_train/pipelines/submit_eval.sh
+++ llm_self_train/pipelines/submit_eval.sh
@@ -6,13 +6,13 @@ for i in {1..5}; do
     --gin_file /home/ruiyiwan/sotopia-llm/llm_self_train/resources/generate.gin \
     --gin_file sotopia_conf/server_conf/server.gin \
     --gin_file sotopia_conf/run_async_server_in_batch.gin \
-    '--gin.ENV_IDS=["01HJPMD5KHA2JT3DNJ9ZGVPBT8"]' \
+    '--gin.ENV_IDS=["01HJPQ34Y3S1TDPTRX1CCH6VPG", "01HJPQ34ZG9WZEDX6BV5QZB1QG"]' \
     '--gin.AGENT1_MODEL="gpt-3.5-turbo"' \
     '--gin.AGENT2_MODEL="custom_model"' \
     '--gin.BATCH_SIZE=4' \
-    '--gin.TAG="pilot-1_checkpoint-base_epoch_1_gpt-3.5-turbo_dev"' \
+    '--gin.TAG="pilot-3_checkpoint_improve-0_epoch-19_gpt-3.5-turbo_dev"' \
     '--gin.PUSH_TO_DB=True' \
-    '--gin.TAG_TO_CHECK_EXISTING_EPISODES="pilot-1_checkpoint-base_epoch_1_gpt-3.5-turbo_dev"'
+    '--gin.TAG_TO_CHECK_EXISTING_EPISODES="pilot-3_checkpoint_improve-0_epoch-19_gpt-3.5-turbo_dev"'
 done
 
 for i in {1..5}; do
@@ -20,17 +20,17 @@ for i in {1..5}; do
     --gin_file /home/ruiyiwan/sotopia-llm/llm_self_train/resources/generate.gin \
     --gin_file sotopia_conf/server_conf/server.gin \
     --gin_file sotopia_conf/run_async_server_in_batch.gin \
-    '--gin.ENV_IDS=["01HJPMD5KHA2JT3DNJ9ZGVPBT8"]' \
+    '--gin.ENV_IDS=["01HJPQ34Y3S1TDPTRX1CCH6VPG", "01HJPQ34ZG9WZEDX6BV5QZB1QG"]' \
     '--gin.AGENT2_MODEL="gpt-3.5-turbo"' \
     '--gin.AGENT1_MODEL="custom_model"' \
     '--gin.BATCH_SIZE=4' \
-    '--gin.TAG="pilot-1_checkpoint-base_epoch_1_gpt-3.5-turbo_dev"' \
+    '--gin.TAG="pilot-3_checkpoint_improve-0_epoch-19_gpt-3.5-turbo_dev"' \
     '--gin.PUSH_TO_DB=True' \
-    '--gin.TAG_TO_CHECK_EXISTING_EPISODES="pilot-1_checkpoint-base_epoch_1_gpt-3.5-turbo_dev"'
+    '--gin.TAG_TO_CHECK_EXISTING_EPISODES="pilot-3_checkpoint_improve-0_epoch-19_gpt-3.5-turbo_dev"'
 done
 
 if [ $? -eq 0 ]; then
-    echo "Success" > /home/ruiyiwan/sotopia-llm/llm_self_train/logs/pilot-1/eval_status_checkpoint-base_epoch_1.txt
+    echo "Success" > /home/ruiyiwan/sotopia-llm/llm_self_train/logs/pilot-3/eval_status_checkpoint_improve-0_epoch-19.txt
 else
-    echo "Failed" > /home/ruiyiwan/sotopia-llm/llm_self_train/logs/pilot-1/eval_status_checkpoint-base_epoch_1.txt
+    echo "Failed" > /home/ruiyiwan/sotopia-llm/llm_self_train/logs/pilot-3/eval_status_checkpoint_improve-0_epoch-19.txt
 fi
