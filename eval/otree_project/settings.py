463f2a2289f81e63eb8feccf6cd0cbccf26aa4cf
Haofei Yu <1125027232@qq.com>
2024-01-12 12:07:41 +0800

    delete game

--- eval/otree_project/settings.py
+++ eval/otree_project/settings.py
@@ -3,12 +3,6 @@ from os import environ
 
 SESSION_CONFIGS = [
     dict(
-        name='guess_two_thirds',
-        display_name="Guess 2/3 of the Average",
-        app_sequence=['guess_two_thirds', 'payment_info'],
-        num_demo_participants=3,
-    ),
-    dict(
         name='survey', 
         app_sequence=['survey', 'payment_info'], 
         num_demo_participants=1
