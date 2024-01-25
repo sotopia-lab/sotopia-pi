94e4afed58917735f782a62e38a5626e37a99158
Haofei Yu <1125027232@qq.com>
2024-01-25 17:22:57 -0500

    support scripts for env fetch from db

--- data_generate/utils/get_env_from_redis.py
+++ data_generate/utils/get_env_from_redis.py
@@ -13,23 +13,28 @@ def get_redisjson_value(r, key):
         print(f"Could not retrieve JSON for key {key}: {e}")
         return None
 
-env_data = {}
 
-try:
-    r = Client(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
-    keys = r.keys('*')
+if __name__ == '__main__':
+    env_data = {}
+    try:
+        r = Client(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+        keys = r.keys('*')
 
-    for key in keys:
-        if 'EnvironmentProfile' in key:
-            value = get_redisjson_value(r, key)
-            env_data[key] = value
+        for key in keys:
+            if 'EnvironmentProfile' in key:
+                value = get_redisjson_value(r, key)
+                env_data[key] = value
 
-except redis.ConnectionError:
-    print("Failed to connect to Redis server.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+    except redis.ConnectionError:
+        print("Failed to connect to Redis server.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-print(len(env_data))
+    print(len(env_data))
 
-with open('./all_environment_profile.json', 'w') as f:
-    json.dump(env_data, f)
+    for data in env_data.values():
+        if data['scenario'] == 'A conversation between two individuals at a charity gala':
+            import pdb; pdb.set_trace()
+
+    with open('./all_environment_profile.json', 'w') as f:
+        json.dump(env_data, f) 
