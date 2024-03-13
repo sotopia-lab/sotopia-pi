6792b4e3c0302233b614b7c6ac0b181284ad6ea6
sharonwx54 <sharon.wx.zhang@gmail.com>
2024-03-13 00:08:54 -0700

    update file

--- data_process/utils/prompt_reverse_engineering.py
+++ data_process/utils/prompt_reverse_engineering.py
@@ -1,18 +1,17 @@
 import argparse
-import enum
-import json
 import os
 from collections import defaultdict
 from typing import Any, Dict, List, Tuple, Union, cast
-
-import numpy as np
+import transformers
 import pandas as pd
 import rich
-import transformers
 from rich.console import Console
 from rich.terminal_theme import MONOKAI
+
 from sotopia.database.logs import EpisodeLog
 from sotopia.messages.message_classes import ActionType
+import numpy as np
+import json
 
 # PROMPT_PREFIX = "Prompt after formatting:\n"
 MAX_TOKEN = 2048  # 5000
@@ -27,7 +26,7 @@ You are at Turn #{turn_number}."""
 
 # PYDANTIC_FORMAT_INSTRUCTIONS.format(schema=schema_str)
 
-FORMAT_TEMPLATE = """ Your available action types are
+FORMAT_TEMPLATE=""" Your available action types are
 "none action speak non-verbal communication leave".
 Note: You can "leave" this conversation if 1. you have achieved your social goals, 2. this conversation makes you uncomfortable, 3. you find it uninteresting/you lose your patience, 4. or for other reasons you want to leave.
 
@@ -38,10 +37,9 @@ the object {\"foo\": [\"bar\", \"baz\"]} is a well-formatted instance of the sch
 \nHere is the output schema:\n```\n{\"description\": \"An interface for messages.\\nThere is only one required method: to_natural_language\", \"properties\": {\"action_type\": {\"title\": \"Action Type\", \"description\": \"whether to speak at this turn or choose to not do anything\", \"enum\": [\"none\", \"speak\", \"non-verbal communication\", \"action\", \"leave\"], \"type\": \"string\"}, \"argument\": {\"title\": \"Argument\", \"description\": \"the utterance if choose to speak, the expression or gesture if choose non-verbal communication, or the physical action if choose action\", \"type\": \"string\"}}, \"required\": [\"action_type\", \"argument\"]}\n```\u001b[0m
 """
 # static
-# " ".join(ActionType)
-ACTION_LIST = "none action speak non-verbal communication leave"
+ACTION_LIST = "none action speak non-verbal communication leave" #" ".join(ActionType)
 
-ACTION_REVERSE_MAP = {"left ": "leave", "did n": "none", "said:": "speak"}
+ACTION_REVERSE_MAP = {"left ": "leave", 'did n': 'none', 'said:': 'speak'}
 
 MODEL_CHECKPOINT = "meta-llama/Llama-2-13b-chat-hf"
 HF_TOKEN = "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"
@@ -102,9 +100,9 @@ def generate_result(msg):
             result["argument"] = msg
         case "non-verbal communication":
             result["argument"] = msg
-
+            
     str_result = json.dumps(result)
-
+        
     return str_result
 
 
@@ -125,19 +123,17 @@ def truncate_prompt_to_length(dia_his, surpass_num, tokenizer=TOKENIZER):
     return trunc_dia
 
 
-def reverse_episode_log(
-    epilog, later_speak=False, include_format=False, max_token=MAX_TOKEN
-):
+def reverse_episode_log(epilog, later_speak=False, include_format=True, max_token=MAX_TOKEN):
     episode_msg = epilog.messages
     # per episode
-    agent_model = epilog.models[1]
+    agent_model = epilog.models[1] if not later_speak else epilog.models[2]
     promt_template = PROMPT_TEMPLATE
 
     if len(episode_msg) > 0:
         init_loop = episode_msg[0]
         # figure out who speak later, as we must use the 2nd player's data, else turn 0 have nothing to predict the beginning
         if later_speak:
-            speaker = init_loop[-1][0]  # this would be the agent as well
+            speaker = init_loop[-1][0] # this would be the agent as well
             turn_div = 1
         # figure out who speak the first
         else:
@@ -151,62 +147,76 @@ def reverse_episode_log(
         msg = episode_msg[i]
         if (len(msg) != 4) and i < (len(episode_msg) - 1):
             continue
-        turn_dic = {"model": agent_model}
+        turn_dic = {"model":agent_model}
         for tpl in msg:
-            if tpl[0] == "Environment" and (tpl[1] == speaker):
+            if (tpl[0] == 'Environment' and (tpl[1] == speaker)):
                 if i > 0:
-                    dial_history += "\n" + tpl[2]
+                    dial_history += "\n"+tpl[2]
                 else:
-                    # for the first context, we don't need \n
+                    # for the first context, we don't need \n 
                     context = tpl[2]
                     dial_history += context
-
-            if (
-                tpl[0] == speaker
-            ):  # if speaker is the agent, use what he said as result
+                       
+            if tpl[0] == speaker: # if speaker is the agent, use what he said as result
                 str_result = generate_result(tpl[2])
-                # check if this is the end
-        if i % 2 == turn_div:
+                # check if this is the end 
+        if i%2 == turn_div:  
             # take alternative turns as we always want to predict one agent, not both
             next_turn = i
             prompt = promt_template.format(
-                agent=speaker, history=dial_history, turn_number=next_turn
-            )
+                    agent=speaker, history=dial_history, turn_number=next_turn)
             over_tokens = surpass_max_token_check(prompt, max_token)
             if over_tokens > 0:
-                all_dial = dial_history[len(context) :]
-                # print(all_dial)
+                all_dial = dial_history[len(context):]
+                #print(all_dial)
                 trun_dial = truncate_prompt_to_length(all_dial, over_tokens)
                 prompt = promt_template.format(
-                    agent=speaker,
-                    history=context + "\n" + trun_dial,
-                    turn_number=next_turn,
-                )
+                    agent=speaker, history=context+"\n"+trun_dial, turn_number=next_turn)
             if include_format:
                 prompt += FORMAT_TEMPLATE
-            turn_dic["prompt"] = prompt
-            turn_dic["result"] = str_result
+            turn_dic["prompt"] = prompt   
+            turn_dic['result'] = str_result
             prompt_result_instances.append(turn_dic)
 
     return prompt_result_instances
 
 
-def parse_prompt_to_json(episode, dir, init_speak, include_format=False):
-    prompt_result_instances = reverse_episode_log(
-        episode, init_speak, include_format
-    )
+def concat_episode_msg(epilog):
+    episode_msg = epilog.messages
+    # per episode
 
+    if len(episode_msg) > 0:
+        init_loop = episode_msg[0]
+        speaker = init_loop[-2][0]
+    dial_history = ""
+
+    for i in range(0, len(episode_msg)):
+        msg = episode_msg[i]
+        if (len(msg) != 4) and i < (len(episode_msg) - 1):
+            continue
+        for tpl in msg:
+            if (tpl[0] == 'Environment' and (tpl[1] == speaker)):
+                if i > 0:
+                    dial_history += "\n"+tpl[2]
+                else:
+                    # for the first context, we don't need \n 
+                    context = tpl[2]
+                    dial_history += context
+                       
+    return dial_history
+
+
+def parse_prompt_to_json(episode, dir, init_speak, include_format=False):
+    prompt_result_instances = reverse_episode_log(episode, init_speak, include_format)
+    
     if not os.path.exists(dir):
         os.makedirs(dir)
 
     for i in range(len(prompt_result_instances)):
         instance = prompt_result_instances[i]
         todump = json.dumps(instance, indent=4)
-        with open(
-            dir + "/{}-{}-{}.json".format(episode.pk, init_speak, i), "w"
-        ) as f:
+        with open(dir+"/{}-{}-{}.json".format(episode.pk, init_speak, i), "w") as f:
             f.write(todump)
-
 
 def run_reverse_by_pk_agent(episode_pk, agent_side, save_dir):
     """
@@ -217,9 +227,9 @@ def run_reverse_by_pk_agent(episode_pk, agent_side, save_dir):
 
 
 def run_all_tag_reverse(filter_env_dic, dir):
-    # tag_episodes = get_clean_episodes(selected_tags=[tag])[tag]
+    #tag_episodes = get_clean_episodes(selected_tags=[tag])[tag]
     for k, v in filter_env_dic.items():
-        cutoff = len(v) // 2
+        cutoff = len(v)//2
         for i in range(len(v)):
             episode = v[i]
             if i < cutoff:
